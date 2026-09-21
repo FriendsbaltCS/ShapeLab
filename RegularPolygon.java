cbb5ff3a6e346679e047d376e4b43793d1d970de
Joel <jhammer@friendsbalt.org>
2026-09-21 19:10:50 -0400

    modify RegularPolygon constructor

--- RegularPolygon.java
+++ RegularPolygon.java
@@ -1,6 +1,14 @@
 public class RegularPolygon extends Polygon {
 
-    public RegularPolygon(double x, double y, int n) {
+    /**
+     * Constructs a regular polygon of order n from a center point and a
+     * vertex.
+     * @param cx the x-coordinate of the center
+     * @param cy the y-coordinate of the center
+     * @param vx the x-coordinate of one vertex
+     * @param vy the y-coordinate of that same vertex
+     */
+    public RegularPolygon(double cx, double cy, double vx, double vy, int n) {
         if (n < 3) {
             throw new IllegalArgumentException("Polygons must have > 2 vertices");
         }
